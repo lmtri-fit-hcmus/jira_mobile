7fcb51503f841ae6040d45c4b106878afd2386fb
baduy09 <nguyenphambaduy0309@gmail.com>
2023-01-03 05:03:21 +0700

    Project(board, backlog): show data queried

--- lib/objects/sprint.dart
+++ lib/objects/sprint.dart
@@ -1,5 +1,6 @@
 import 'package:jira_mobile/objects/comment.dart';
 import 'package:jira_mobile/objects/issue.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 
 class Sprint {
   String _id;
@@ -13,4 +14,40 @@ class Sprint {
 
   Sprint(this._id, this._epic_id, this._name, this._goal, this._status,
       this._start_date, this._due_date, this._list_issue);
+}
+
+class SprintModel {
+  ObjectId? _id;
+  ObjectId? project;
+  String? name;
+  String? goal;
+  String? status;
+  Timestamp? startDate;
+  Timestamp? dueDate;
+
+  ObjectId? get getId => _id;
+
+  SprintModel();
+
+  fromJson(Map json) {
+    _id = json["_id"];
+    name = json["name"];
+    project = json["project"];
+    startDate = json["startDate"];
+    goal = json["goal"];
+    status = json["status"];
+    dueDate = json["dueDate"];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      '_id': _id,
+      'project': project,
+      'name': name,
+      'goal': goal,
+      'status': status,
+      'dueDate': dueDate,
+      'startDate': startDate,
+    };
+  }
 }
